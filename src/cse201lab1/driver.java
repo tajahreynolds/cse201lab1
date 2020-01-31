d8d9ddcd5ee124acaa02a49fc521d7dc4fc7699f
TaJah R <reynolt4@miamioh.edu>
2020-01-31 16:02:50 -0500

    searchTop() -> search(), finished user interaction in main method.

--- src/cse201lab1/driver.java
+++ src/cse201lab1/driver.java
@@ -11,6 +11,7 @@
 package cse201lab1;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -18,7 +19,7 @@ import java.util.Scanner;
 public class driver {
 
 	public static void main(String[] args) {
-		ArrayList<String> namesList;
+		ArrayList<String> namesList = new ArrayList<String>();
 		String gender = "non-binary";
 		Scanner in = new Scanner(System.in);
 		boolean exit = false, valid = false;
@@ -33,42 +34,63 @@ public class driver {
 			// Load BoyNames.txt or GirlNames.txt based on user input
 			while (!valid) {
 				System.out.print("Enter 1 to load boy names, or enter 2 to load girl names: ");
+				try {
+					switch (in.nextInt()) {
+					case 1:
+						namesList = readFile(boyNames);
+						gender = "boy";
+						valid = true;
+						break;
 
-				switch (in.nextInt()) {
-				case 1:
-					namesList = readFile(boyNames);
-					gender = "boy";
-					valid = true;
-					break;
-
-				case 2:
-					namesList = readFile(girlNames);
-					gender = "girl";
-					valid = true;
-					break;
-				default:
-					System.out.println("The entry was not recognized. Please try again.");
-					break;
+					case 2:
+						namesList = readFile(girlNames);
+						gender = "girl";
+						valid = true;
+						break;
+					default:
+						System.out.println("The entry was not recognized. Please try again.");
+						break;
+					}
+				} catch (InputMismatchException e) {
+					System.out.println("Invalid entry. The program will now exit.");
+					in.close();
+					System.exit(1);
 				}
 			}
-			
+
 			// Search for the name that the user enters
 			System.out.print("Please enter a " + gender + " name: ");
 			String nameInput = in.next();
-			
+			search(namesList, nameInput);
+			exit = true;
 		}
 
 	}
-	
+
 	/**
-	 * Searches for a string in the first ten entries of an ArrayList<String>
+	 * Searches for a string in an ArrayList<String>
 	 * 
 	 * @param list The ArrayList<String> to be searched
-	 * @param str The string to be searched for in the list.
+	 * @param str  The string to be searched for in the list.
 	 */
-	private static void searchTop(ArrayList<String> list, String str) {
-		// TODO
+	private static void search(ArrayList<String> list, String str) {
+		String modStr = str.substring(0, 1).toUpperCase() + str.substring(1);
+		boolean found = false;
+		for (String s : list) {
+			if (s.equalsIgnoreCase(str)) {
+				System.out.println("The name " + s + " is one of the most popular names.");
+				return;
+			}
+			if (s.contains(str) || s.contains(modStr)) {
+				System.out.println("The name " + str + " is contained in one of the most popular names, " + s + ".");
+				found = true;
+			}
+		}
+		if (!found) {
+			System.out.println("The name " + str + " was not found.");
+		}
 	}
+
 	/**
 	 * Reads the contents of a file into an ArrayList
 	 * 
